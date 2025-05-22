25082a08e205d67fc43fb2ca476206b5a998aea2
Juan Pablo <juanpi17@gmail.com>
2025-05-22 17:57:47 -0300

    chore: format droppable area

--- src/app/_components/droppableCanvas/index.tsx
+++ src/app/_components/droppableCanvas/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {useDroppable} from '@dnd-kit/core';
 
-export function Droppable(props: React.PropsWithChildren) {
+export function DroppableCanvas(props: React.PropsWithChildren) {
   const {isOver, setNodeRef} = useDroppable({
-    id: 'droppable',
+    id: 'droppableCanvas',
   });
   const style = {
     color: isOver ? 'green' : undefined,
